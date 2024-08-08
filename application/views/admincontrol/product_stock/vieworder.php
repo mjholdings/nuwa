d1d408dcf2451b2af3534d6809c7dee0b88c4ef8
Johnathan <duongcgc@gmail.com>
2024-08-08 11:16:01 +0700

    a

--- application/views/admincontrol/product_stock/vieworder.php
+++ application/views/admincontrol/product_stock/vieworder.php
@@ -20,48 +20,7 @@
 			<h4 class="mb-4"><?= __('Đơn nhập') ?> : (<?= orderId($order['id']) ?>)</h4>
 			<p><i class="mdi mdi-calendar-text"></i> <?= __('admin.date') ?> : <?php echo date("m-j-Y h:i A", strtotime($order['created_at'])); ?></p>
 
-			<h5 class="mt-3"><?= __('admin.product_info') ?></h5>
-			<div class="table-responsive">
-				<table class="table table-striped">
-					<thead>
-						<tr>
-							<th colspan="2"><?= __('admin.name') ?></th>
-							<th><?= __('admin.unit_price') ?></th>
-							<th><?= __('admin.quantity') ?></th>
-							<th><?= __('admin.total') ?></th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $total_order = 0;
-						foreach ($order['products'] as $product) { ?>
-							<?php $product_featured_image = base_url('assets/images/product/upload/thumb/' . $product['product_featured_image']); ?>
-							<tr colspan="2">
-								<td width="50px"><img src="<?= $product_featured_image ?>" class="img-fluid" onerror="this.onerror=null;this.src='<?= base_url('assets/images/no_image_available.png') ?>';"></td>
-								<td>
-									<h5>
-										<?= $product['product_name'] ? $product['product_name'] : '<i class="text-muted">' . __('admin.product_not_available') . '</i>' ?> <?= ($combinationString != "") ? "(" . $combinationString . ")" : "" ?>
-									</h5>
-								</td>
-								<td><?php echo c_format($product['product_price']); ?></td>
-								<td><?php echo $product['stock_quantity']; ?></td>
-								<td><?php echo c_format($product['product_price'] * $product['stock_quantity']);
-									$total_order += $product['product_price'] * $product['stock_quantity']; ?></td>
-							</tr>
-						<?php } ?>
-						<tr>
-							<td colspan="3"></td>
-							<td><?= 'Tổng giá trị nhập hàng: ' ?></td>
-							<td><b><?php echo c_format($total_order); ?></b></td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
-
-<div class="row">
+			<div class="row">
 	<div class="col-lg-8">
 		<div class="card mb-3">
 			<div class="card-header border-0">
@@ -144,6 +103,49 @@
 	</div>
 
 </div>
+
+
+			<h5 class="mt-3"><?= __('admin.product_info') ?></h5>
+			<div class="table-responsive">
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th colspan="2"><?= __('admin.name') ?></th>
+							<th><?= __('admin.unit_price') ?></th>
+							<th><?= __('admin.quantity') ?></th>
+							<th><?= __('admin.total') ?></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php $total_order = 0;
+						foreach ($order['products'] as $product) { ?>
+							<?php $product_featured_image = base_url('assets/images/product/upload/thumb/' . $product['product_featured_image']); ?>
+							<tr colspan="2">
+								<td width="50px"><img src="<?= $product_featured_image ?>" class="img-fluid" onerror="this.onerror=null;this.src='<?= base_url('assets/images/no_image_available.png') ?>';"></td>
+								<td>
+									<h5>
+										<?= $product['product_name'] ? $product['product_name'] : '<i class="text-muted">' . __('admin.product_not_available') . '</i>' ?> <?= ($combinationString != "") ? "(" . $combinationString . ")" : "" ?>
+									</h5>
+								</td>
+								<td><?php echo c_format($product['product_price']); ?></td>
+								<td><?php echo $product['stock_quantity']; ?></td>
+								<td><?php echo c_format($product['product_price'] * $product['stock_quantity']);
+									$total_order += $product['product_price'] * $product['stock_quantity']; ?></td>
+							</tr>
+						<?php } ?>
+						<tr>
+							<td colspan="3"></td>
+							<td><?= 'Tổng giá trị nhập hàng: ' ?></td>
+							<td><b><?php echo c_format($total_order); ?></b></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
 
 <div class="modal fade" id="myModal" role="dailog">
 	<div class="modal-dialog">
